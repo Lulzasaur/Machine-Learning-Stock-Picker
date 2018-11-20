7746e4d7f42088545fe5d709a1c5c6757c0f8df6
JUAN ARECES <arecesj@gmail.com>
2018-11-20 11:06:22 -0800

    added singular frontend route to /

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,31 +1,27 @@
-/**
- * COMPONENT HIERARCHY:
- * https://docs.google.com/drawings/d/1YpgTfRmAHL9connFeR6cSBvD4-hMTtZACfN0oWHIxFQ/edit?usp=sharing
- * 
- */
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, NavLink, Switch } from 'react-router-dom';
+import DisplayContainer from '../DisplayContainer/DisplayContainer';
+
+/** Overall Project Silas application:
+ *
+ * - shows header, nav links, and contains routes to:
+ *   - Chart page
+ *
+ * COMPONENT HIERARCHY:
+ * https://docs.google.com/drawings/d/1YpgTfRmAHL9connFeR6cSBvD4-hMTtZACfN0oWHIxFQ/edit?usp=sharing
+ *
+ */
 
 class App extends Component {
   render() {
-    ummm reee?
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {/* Router */}
+        <Switch>
+          <Route eact path="/" render={() => <DisplayContainer />} />
+        </Switch>
       </div>
     );
   }
