a40dec319aaaa63f3f5bc32b868177d117b71a1c
Hasier Pastor <hasier.pastor@gmail.com>
2018-11-20 16:50:02 -0800

    backend returns json object with 0 and 1 keys representing probabilities

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, redirect, render_template, jsonify, session
 # from flask_debugtoolbar import DebugToolbarExtension
 from Model import MlModel
 import requests
+import numpy
 from KEY import API_SECRET_KEY
 
 app = Flask(__name__)
@@ -14,9 +15,6 @@ def getStockPrediction(ticker):
     """Depending on ticker route will make a get request to API to get data on that stock,
     pass that data into the ML model and return prediction data in JSON format to front end
     """
-
-
-
     api_resp = requests.get(f'{BASE_URL}',
         params={
             "function":"TIME_SERIES_DAILY",
@@ -26,7 +24,13 @@ def getStockPrediction(ticker):
         })
     stock_data = api_resp.json()
     training_data = MlModel.clean_data(stock_data['Time Series (Daily)'],'float')
-    prediction_data = MlModel.run_model(training_data)
+    predictions = MlModel.run_model(training_data)
+    #turn into own function
+    predictionData = {
+        '0': int(predictions[0]*100),
+        '1': int(predictions[1]*100)
+    }
 
-    return prediction_data
+    return jsonify(predictionData)
+
 
